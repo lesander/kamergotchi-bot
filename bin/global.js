2195fbe5d07397641a495ace7c478130396e5983
Sander Laarhoven <sander@laarhoven.org>
2017-02-22 23:10:32 +0100

    Show nice usage message if no argument is provided.

--- bin/global.js
+++ bin/global.js
@@ -7,5 +7,11 @@ console.log('KamerBOTchi v'+require('../package.json').version)
 console.log('Developed with <3 by Sander Laarhoven')
 console.log('https://git.io/kamergotchi')
 console.log('\nPlayer token is set to ' + process.argv[2])
+
+if (typeof process.argv[2] === 'undefined' || !process.argv[2]) {
+  console.log('No player token given.\nUsage: `kamergotchi [playerToken]`')
+  process.exit()
+}
+
 kamerbotchi.setToken(process.argv[2])
 kamerbotchi.init()
