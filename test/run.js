8ec0f3a97f8ae1d6aaefc38ca2fed28797785307
Sander Laarhoven <sander@laarhoven.org>
2017-02-22 19:22:30 +0100

    Add more tests.

---
+++ test/run.js
@@ -0,0 +1,14 @@
+/**
+ * kamergotchi-bot v1.0.0
+ * By Sander Laarhoven
+ * Licensed under the MIT License
+ * https://git.io/kamergotchi
+ */
+
+const kamerbotchi = require('../index.js')
+
+async function init() {
+  kamerbotchi.init()
+}
+
+init()
